a8fd3d5345b8f825f119144905623f0b18317530
Huy <diepngochuy3@gmail.com>
2023-09-18 16:52:24 +0700

    setUpEnv

---
+++ Server/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import bcrypt from 'bcrypt';
+import { jwt } from 'jsonwebtoken';
+
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+const con = mysql.createConnection({
+    host: "localhost",
+    user: "",
+    password: "250114",
+})
+
+
+con.connect(function (err) {
+    if (err) {
+        console.log("Error in connection");
+    } else {
+        console.log("Connected");
+    }
+
+})
+
+app.listen(8081, () => {
+    console.log("Running");
+})
