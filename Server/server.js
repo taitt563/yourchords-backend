0e0a45aee33a8ffd0072ea42b72a83dcdfa42b3a
Huy <diepngochuy3@gmail.com>
2023-09-21 17:44:35 +0700

    update code server

--- Server/server.js
+++ Server/server.js
@@ -4,6 +4,8 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import multer from 'multer';
+import path from 'path';
 
 const app = express();
 app.use(cors());
@@ -14,7 +16,7 @@ const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
-    database: "signup"
+    database: "yourchord"
 })
 
 
@@ -25,6 +27,18 @@ con.connect(function (err) {
         console.log("Connected");
     }
 })
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname));
+    }
+})
+const upload = multer({
+    storage: storage
+})
+
 
 app.post('/login', (req, res) => {
     const sql = "SELECT * FROM login Where username = ? AND  password = ?";
@@ -36,6 +50,36 @@ app.post('/login', (req, res) => {
             return res.json({ Status: "Error", Error: "Wrong Username or Password" });
         }
     })
+})
+app.post('/signin', (req, res) => {
+    const sql = "INSERT INTO login (username, password) VALUES (?, ?)";
+    con.query(sql, [req.body.username, req.body.password], (err, result) => {
+        if (err) return res.json({ Status: "Error", Error: "Error in runnig query" });
+        if (result.length > 0) {
+            return res.json({ Status: "Success" })
+        } else {
+            return res.json({ Status: "Error", Error: "Error" });
+        }
+    })
+})
+app.post('/createSong', upload.single('image'), (req, res) => {
+    console.log(req.file);
+    // const sql = "INSERT INTO song (`name`,`image`,`date`) VALUES (?)";
+    // bcrypt.hash(req.body.password.toString(), 10, (err, hash) => {
+    //     if(err) return res.json({Error: "Error in hashing password"});
+    //     const values = [
+    //         req.body.name,
+    //         req.body.email,
+    //         hash,
+    //         req.body.address,
+    //         req.body.salary,
+    //         req.file.filename
+    //     ]
+    //     con.query(sql, [values], (err, result) => {
+    //         if(err) return res.json({Error: "Inside singup query"});
+    //         return res.json({Status: "Success"});
+    //     })
+    // } )
 })
 app.listen(8081, () => {
     console.log("Running");
