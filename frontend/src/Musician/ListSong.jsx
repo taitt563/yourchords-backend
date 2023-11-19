1f5ded14db6a674dfb1f5a0edc4d2069b598223c
Tu <vctu2000@gmail.com>
2023-11-19 08:09:17 +0700

    update UI listSong

--- frontend/src/Musician/ListSong.jsx
+++ frontend/src/Musician/ListSong.jsx
@@ -1,3 +1,4 @@
+
 import SearchAppBar from '../component/SearchAppBar';
 // import Button from '@mui/material/Button';
 // import CloudUploadIcon from '@mui/icons-material/CloudUpload';
@@ -6,16 +7,23 @@ function ListSong() {
 
     return (
         <>
-            <div className="card mx-3 my-5">
-                <div className="card-body">
-                    <p className='headP'>Lạ lùng</p> <p className='headP'>- Vũ</p>
-                    <p>Description</p>
-                    <p className='headP'>Pic</p>
-                    <p className='headP'>-Vũ</p>
-                    <p className='headP'>-Ngày tháng năm</p>
-                    <p className='headP' style={{ marginLeft: '50px' }}>Hợp âm</p>
-                    <p className='headP' style={{ marginLeft: '50px' }}>Lượt xem</p>
+            <div style={{ display: 'flex' }}>
+                <div className="card mx-3 my-5" style={{ width: '50%' }}>
+                    <div className="card-body" >
+                        <p className='headP'>Lạ lùng</p> <p className='headP'>- Vũ</p>
+                        <p>Description</p>
+                        <p className='headP'>Pic</p>
+                        <p className='headP'>-Vũ</p>
+                        <p className='headP'>-Ngày tháng năm</p>
+                        <p className='headP' style={{ marginLeft: '50px' }}>hợp âm</p>
+                        <p className='headP' style={{ marginLeft: '50px' }}>Lượt xem</p>
+                    </div>
                 </div>
+                <div className="card mx-3 my-5" style={{ width: '50%', marginRight: '20px' }}>
+                    <p style={{ textAlign: 'center' }}>Thể loại</p>
+                    <div style={{ display: 'flex', backgroundColor: 'grey', width: '100px' }}><p>Nhac tre </p><p> (so bai``)</p></div>
+                </div>
+
             </div>
         </>
     )
