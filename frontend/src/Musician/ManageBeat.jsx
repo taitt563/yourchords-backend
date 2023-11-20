2ba0474cfb09b18570dc3454bdda15ca76f173eb
Tu <vctu2000@gmail.com>
2023-11-21 00:23:29 +0700

    update manageBeat

--- frontend/src/Musician/ManageBeat.jsx
+++ frontend/src/Musician/ManageBeat.jsx
@@ -5,11 +5,11 @@ import { Link } from 'react-router-dom'
 import React, { useEffect, useState } from 'react';
 
 
+
 function ManageBeat() {
     const [randomValue, setRandomValue] = useState(null);
 
     useEffect(() => {
-        // Gọi API để lấy thông tin ngẫu nhiên
         const fetchData = async () => {
             try {
                 const response = await fetch('YOUR_API_ENDPOINT');
@@ -30,14 +30,12 @@ function ManageBeat() {
                     <h3 className="d-flex justify-content-center">MANAGE BEAT</h3>
                 </div>
                 <div className="d-grid">
-                    {/* <Link to={`/TransactionHistory/${randomValue}`} className="item-grid item-1"> */}
-                    <Link to="/ListSong" className="item-grid item-1">
+                    <Link to="/ListSong" className="item-grid">
                         <h3>Go to ListSong</h3>
                         <h6>20135 bài</h6>
                     </Link>
                 </div>
-
-            </div >
+            </div>
         </>
     )
 }
