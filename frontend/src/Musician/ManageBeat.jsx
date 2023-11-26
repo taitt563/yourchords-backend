2e6701d51c0979997e49cd497e434daa9cf4699a
Tu <vctu2000@gmail.com>
2023-11-26 21:53:00 +0700

    update UI manageBeat

--- frontend/src/Musician/ManageBeat.jsx
+++ frontend/src/Musician/ManageBeat.jsx
@@ -1,27 +1,10 @@
+
 import SearchAppBar from '../component/SearchAppBar';
 // import Button from '@mui/material/Button';
 // import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-import { Link } from 'react-router-dom'
-import React, { useEffect, useState } from 'react';
-
 
 
 function ManageBeat() {
-    const [randomValue, setRandomValue] = useState(null);
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('YOUR_API_ENDPOINT');
-                const data = await response.json();
-                setRandomValue(data.randomValue);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
-
-        fetchData();
-    }, []);
     return (
         <>
             <SearchAppBar />
@@ -30,11 +13,70 @@ function ManageBeat() {
                     <h3 className="d-flex justify-content-center">MANAGE BEAT</h3>
                 </div>
                 <div className="d-grid">
-                    <Link to="/ListSong" className="item-grid">
-                        <h3>Go to ListSong</h3>
-                        <h6>20135 bài</h6>
-                    </Link>
+                    <div className="list-grid">
+                        <div className="item-grid item-1">
+                            <h3>Ballad</h3>
+                            <h6>20135 bài</h6>
+                        </div>
+                        <div className="item-grid item-2">
+                            <h3>Blues Tune</h3>
+                            <h6>3121 bài</h6>
+                        </div>
+                        <div className="item-grid item-3">
+                            <h3>Disco Tune</h3>
+                            <h6>1701 bài</h6>
+                        </div>
+                        <div className="item-grid item-4">
+                            <h3>Slow Tune</h3>
+                            <h6>1531 bài</h6>
+                        </div>
+                        <div className="item-grid item-5">
+                            <h3>Bollero Tune</h3>
+                            <h6>1258 bài</h6>
+                        </div>
+                        <div className="item-grid item-6">
+                            <h3>Fox Tune</h3>
+                            <h6>749 bài</h6>
+                        </div>
+                        <div className="item-grid item-7">
+                            <h3>Valse Tune</h3>
+                            <h6>697 bài</h6>
+                        </div>
+                        <div className="item-grid item-8">
+                            <h3>Tango Tune</h3>
+                            <h6>20135 bài</h6>
+                        </div>
+                        <div className="item-grid item-9">
+                            <h3>Pop Tune</h3>
+                            <h6>439 bài</h6>
+                        </div>
+                        <div className="item-grid item-10">
+                            <h3>Boston Tune</h3>
+                            <h6>435 bài</h6>
+                        </div>
+                        <div className="item-grid item-11">
+                            <h3>Waltz</h3>
+                            <h6>375 bài</h6>
+                        </div>
+                        <div className="item-grid item-12">
+                            <h3>Chachacha dance</h3>
+                            <h6>368 bài</h6>
+                        </div>
+                        <div className="item-grid item-13">
+                            <h3>Rock Tune</h3>
+                            <h6>366 bài</h6>
+                        </div>
+                        <div className="item-grid item-14">
+                            <h3>Dhumba Dance</h3>
+                            <h6>213 bài</h6>
+                        </div>
+                        <div className="item-grid item-15">
+                            <h3>Bossa Nova</h3>
+                            <h6>163 bài</h6>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </>
     )
