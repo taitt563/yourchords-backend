12bf5d2370f26ffe9bb35ee67ba9eaf8d525018f
Huy <diepngochuy3@gmail.com>
2023-12-02 12:47:18 +0700

    update code UI feature payment

--- frontend/src/Customer/OrderCustomer.jsx
+++ frontend/src/Customer/OrderCustomer.jsx
@@ -49,117 +49,115 @@ function OrderCustomer() {
   return (
     <>
       <SearchAppBar />
-      <div className="maincontainer">
-        <div className="container">
-          <div className="py-5 text-center">
-            <h2 style={{ color: '#0d6efd', fontWeight: 'bold' }}>Order</h2>
-          </div>
-          <div className="row">
-            <div className="col-md-4 order-md-2 mb-4" style={{ backgroundColor: "#EFFBEF", height: '270px' }}>
-              <h4 className="d-flex justify-content-between align-items-center mb-3">
-                <span className="text-muted">Notes:</span>
-              </h4>
-              <ul className="list-group mb-3">
-                <div className='notes' style={{ marginLeft: '50px' }}>
-                  <li>Posts are not duplicated</li>
-                  <li>Write the full name of the song</li>
-                  <li>Type in English or Vietnamese with accents</li>
-                  <li>Enter full lyrics and chords. Avoid using "similar", "as above"...</li>
-                  <li>Do not post songs with reactionary or sensitive content that violate Vietnamese customs and traditions.</li>
-                </div>
-              </ul>
-            </div>
-
-            <div className="col-md-8 order-md-1">
-              <form className="needs-validation" noValidate onSubmit={handleConfirmOrder}>
-                <div className="row">
-                  <div className="mb-3">
-                    <label htmlFor="title">Song title</label>
-                    <input type="text" className="form-control" id="title" placeholder="I see fire" value={title} onChange={(e) => setTitle(e.target.value)} required />
-                    <div className="invalid-feedback">
-                      Song title is required.
-                    </div>
-                  </div>
-                </div>
-
-                <div className="mb-3">
-                  <label htmlFor="lyric">Lyric</label>
-                  <div className="input-group">
-                    <textarea id='lyric' rows={15} cols={100} value={lyric} onChange={(e) => setLyric(e.target.value)} style={{ borderRadius: '10px' }} required></textarea>
-                    <div className="invalid-feedback">
-                      Lyric is required.
-                    </div>
-                  </div>
-                </div>
-
-                <div className="mb-3">
-                  <label htmlFor="artist">Artist</label>
-                  <input type="text" className="form-control" id="artist" placeholder="Ed sheeran" value={artist} onChange={(e) => setArtist(e.target.value)} required />
-                  <div className="invalid-feedback">
-                    Please enter artist!
-                  </div>
-                </div>
-
-                <div className="mb-3">
-                  <label htmlFor="duration" className="form-label">Duration</label>
-                  <div className="input-group">
-                    <DatePicker
-                      id="duration"
-                      selected={duration}
-                      onChange={(date) => setDuration(date)}
-                      showTimeSelect
-                      timeFormat="HH:mm"
-                      timeIntervals={15}
-                      timeCaption="time"
-                      dateFormat="yyyy-MM-dd HH:mm"
-                      className="form-control"
-                      required
-                    />
-                    <div >
-                      <span className="input-group-text bg-primary  text-white">
-                        <i className="bi-calendar-day"></i>
-                      </span>
-                    </div>
-                    <div className="invalid-feedback">
-                      Please enter duration!
-                    </div>
-                  </div>
-                </div>
-
-                <div className="row">
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="cc-genre">Genre</label>
-                    <select className="form-control" id="cc-genre" value={genre} onChange={(e) => setGenre(e.target.value)} required>
-                      <option value="">Select Genre</option>
-                      <option value="acoustic">Acoustic</option>
-                      <option value="rock">Rock</option>
-                      <option value="jazz">Jazz</option>
-                      <option value="ballad">Ballad</option>
-                      <option value="pop">Pop</option>
-                      <option value="rb">R&b</option>
-                    </select>
-                    <div className="invalid-feedback">
-                      Please enter genre!
-                    </div>
-                  </div>
-                  <div className="col-md-6 mb-3">
-                    <label htmlFor="cc-link">Link</label>
-                    <input type="text" className="form-control" id="cc-link" placeholder="" value={link} onChange={(e) => setLink(e.target.value)} required />
-                    <div className="invalid-feedback">
-                      Link is required!
-                    </div>
-                  </div>
-                </div>
-                <hr className="mb-4" />
-                <button className="btn btn-primary btn-lg btn-block" type="submit">Confirm order</button>
-              </form>
-            </div>
-          </div>
-
-          <footer className="my-5 pt-5 text-muted text-center text-small">
-
-          </footer>
+      <div className="container">
+        <div className="py-4 text-center">
+          <h2 style={{ color: '#0d6efd', fontWeight: 'bold' }}>Order</h2>
         </div>
+        <div className="row">
+          <div className="col-md-4 order-md-2 mb-4" style={{ backgroundColor: "#EFFBEF", height: '270px' }}>
+            <h4 className="text-center mb-3">
+              <span>Notes</span>
+            </h4>
+            <ul className="list-group mb-3">
+              <div className='notes' style={{ marginLeft: '50px' }}>
+                <li>Posts are not duplicated</li>
+                <li>Write the full name of the song</li>
+                <li>Type in English or Vietnamese with accents</li>
+                <li>Enter full lyrics and chords. Avoid using "similar", "as above"...</li>
+                <li>Do not post songs with reactionary or sensitive content that violate Vietnamese customs and traditions.</li>
+              </div>
+            </ul>
+          </div>
+
+          <div className="col-md-8 order-md-1">
+            <form className="needs-validation" noValidate onSubmit={handleConfirmOrder}>
+              <div className="row">
+                <div className="mb-3">
+                  <label htmlFor="title">Song title</label>
+                  <input type="text" className="form-control" id="title" placeholder="I see fire" value={title} onChange={(e) => setTitle(e.target.value)} required />
+                  <div className="invalid-feedback">
+                    Song title is required.
+                  </div>
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="lyric">Lyric</label>
+                <div className="input-group">
+                  <textarea id='lyric' rows={15} cols={100} value={lyric} onChange={(e) => setLyric(e.target.value)} style={{ borderRadius: '10px' }} required></textarea>
+                  <div className="invalid-feedback">
+                    Lyric is required.
+                  </div>
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="artist">Artist</label>
+                <input type="text" className="form-control" id="artist" placeholder="Ed sheeran" value={artist} onChange={(e) => setArtist(e.target.value)} required />
+                <div className="invalid-feedback">
+                  Please enter artist!
+                </div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="duration" className="form-label">Duration</label>
+                <div className="input-group">
+                  <DatePicker
+                    id="duration"
+                    selected={duration}
+                    onChange={(date) => setDuration(date)}
+                    showTimeSelect
+                    timeFormat="HH:mm"
+                    timeIntervals={15}
+                    timeCaption="time"
+                    dateFormat="yyyy-MM-dd HH:mm"
+                    className="form-control"
+                    required
+                  />
+                  <div >
+                    <span className="input-group-text bg-primary  text-white">
+                      <i className="bi-calendar-day"></i>
+                    </span>
+                  </div>
+                  <div className="invalid-feedback">
+                    Please enter duration!
+                  </div>
+                </div>
+              </div>
+
+              <div className="row">
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="cc-genre">Genre</label>
+                  <select className="form-control" id="cc-genre" value={genre} onChange={(e) => setGenre(e.target.value)} required>
+                    <option value="">Select Genre</option>
+                    <option value="acoustic">Acoustic</option>
+                    <option value="rock">Rock</option>
+                    <option value="jazz">Jazz</option>
+                    <option value="ballad">Ballad</option>
+                    <option value="pop">Pop</option>
+                    <option value="rb">R&b</option>
+                  </select>
+                  <div className="invalid-feedback">
+                    Please enter genre!
+                  </div>
+                </div>
+                <div className="col-md-6 mb-3">
+                  <label htmlFor="cc-link">Link</label>
+                  <input type="text" className="form-control" id="cc-link" placeholder="" value={link} onChange={(e) => setLink(e.target.value)} required />
+                  <div className="invalid-feedback">
+                    Link is required!
+                  </div>
+                </div>
+              </div>
+              <hr className="mb-4" />
+              <button className="btn btn-primary btn-lg btn-block" type="submit">Confirm order</button>
+            </form>
+          </div>
+        </div>
+
+        <footer className="my-5 pt-5 text-muted text-center text-small">
+
+        </footer>
       </div>
     </>
   )
--- frontend/src/Musician/ViewOrderMusician.jsx
+++ frontend/src/Musician/ViewOrderMusician.jsx
@@ -5,12 +5,20 @@ import SearchAppBar from '../component/SearchAppBar';
 import "react-datepicker/dist/react-datepicker.css";
 import axios from 'axios';
 import moment from 'moment';
+import Stack from '@mui/material/Stack';
+import Alert from '@mui/material/Alert';
 function ViewOrderMusician() {
     const [orderData, setOrderData] = useState([]);
     const apiUrl = import.meta.env.VITE_REACT_APP_API_URL;
     const token = sessionStorage.getItem('token');
     const userId = token.split(':')[0];
     const { id } = useParams();
+    const [docxFile, setDocxFile] = useState(null);
+    const [imageFile, setImageFile] = useState(null);
+    const [openErrorDocx, setOpenErrorDocx] = useState(false);
+    const [openErrorImage, setOpenErrorImage] = useState(false);
+    const [docxFileName, setDocxFileName] = useState(null);
+    const [imageFileName, setImageFileName] = useState(null);
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -30,77 +38,184 @@ function ViewOrderMusician() {
         fetchOrderData();
     }, [apiUrl, userId]);
 
+    const handleDocxFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file && file.type === 'application/vnd.openxmlformats-officedocument.wordprocessingml.document') {
+            setDocxFile(file);
+            setDocxFileName(file.name);
+        } else {
+            setOpenErrorDocx(true);
+            setTimeout(() => {
+                setOpenErrorDocx(false);
+            }, 3000);
+        }
+    };
 
+    const handleImageFileChange = (event) => {
+        const file = event.target.files[0];
+        if (file && file.type.startsWith('image/')) {
+            setImageFile(file);
+            setImageFileName(file.name);
+        } else {
+            setOpenErrorImage(true);
+            setTimeout(() => {
+                setOpenErrorImage(false);
+            }, 3000);
+        }
+    };
+
+    const handleLyricChange = (event, index) => {
+        const updatedOrderData = [...orderData];
+        updatedOrderData[index].lyric = event.target.value;
+        setOrderData(updatedOrderData);
+    };
     return (
         <>
             <SearchAppBar />
-            <div className="maincontainer">
-                <div className="container">
-                    <div className="py-5 text-center">
-                        <h2 style={{ color: '#0d6efd', fontWeight: 'bold' }}>Order</h2>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-12 order-md-1">
-                            {orderData.map((order, index) => (
-                                <div key={index}>
-                                    <form className="needs-validation" noValidate>
-                                        <div className="row">
-                                            <div className="mb-3">
-                                                <b htmlFor="title">Song title</b>
-                                                <p>{order.song_name}</p>
-                                            </div>
-                                        </div>
+            {openErrorDocx && (
+                <Stack sx={{ width: '100%' }} spacing={2}>
+                    <Alert severity="error">
+                        Invalid file type. Please upload a DOCX file or an image.
+                    </Alert>
+                </Stack>
+            )}
+            {openErrorImage && (
+                <Stack sx={{ width: '100%' }} spacing={2}>
+                    <Alert severity="error">
+                        Invalid file type. Please upload an Image.
+                    </Alert>
+                </Stack>
+            )}
 
-                                        <div className="mb-3">
-                                            <b htmlFor="lyric">Lyric</b>
-                                            <div className="input-group">
-                                                <textarea
-                                                    id="lyric"
-                                                    rows={15}
-                                                    cols={200}
-                                                    value={order.lyric}
-                                                    style={{ borderRadius: '10px' }}
-                                                    required
-                                                >{order.lyric}</textarea>
-                                                <div className="invalid-feedback">
-                                                    Lyric is required.
-                                                </div>
-                                            </div>
-                                        </div>
-
-
-                                        <div className="mb-3">
-                                            <b htmlFor="artist">Artist</b>
-                                            <p>{order.artist_name}</p>
-                                        </div>
-
-                                        <div className="mb-3">
-                                            <b htmlFor="duration" className="form-label">Duration</b>
-                                            <p>{moment(order.duration).format('YYYY - MM - DD  HH:mm:ss')}</p>
-                                        </div>
-
-                                        <div className="row">
-                                            <div className="col-md-6 mb-3">
-                                                <b htmlFor="cc-genre">Genre</b>
-                                                <p>{order.genre}</p>
-                                            </div>
-                                            <div className="col-md-6 mb-3">
-                                                <b htmlFor="cc-link">Link</b>
-                                                <p>{order.link}</p>
-                                            </div>
-                                        </div>
-                                        <hr className="mb-4" />
-                                        <button className="btn btn-primary btn-lg btn-block" onClick={() => navigate("/orderMusician")}>Close</button>
-                                    </form>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-
-                    <footer className="my-5 pt-5 text-muted text-center text-small">
-
-                    </footer>
+            <div className="container">
+                <div className="py-4 text-center">
+                    <h2 style={{ color: '#0d6efd', fontWeight: 'bold' }}>Order</h2>
                 </div>
+                <div className="row">
+                    <div className="col-md-4 order-md-2 " style={{ backgroundColor: "#EFFBEF", height: '270px' }}>
+                        <h4 className="text-center mb-3">
+                            <span>Notes</span>
+                        </h4>
+                        <ul className="list-group mb-3">
+                            <div className='notes' style={{ marginLeft: '50px' }}>
+                                <li>Posts are not duplicated</li>
+                                <li>Write the full name of the song</li>
+                                <li>Type in English or Vietnamese with accents</li>
+                                <li>Enter full lyrics and chords. Avoid using "similar", "as above"...</li>
+                                <li>Do not post songs with reactionary or sensitive content that violate Vietnamese customs and traditions.</li>
+                            </div>
+                        </ul>
+                    </div>
+                    <div className="col-md-8 order-md-1">
+                        {orderData.map((order, index) => (
+                            <div key={index}>
+                                <form className="needs-validation" noValidate>
+                                    <div className="row">
+                                        <div className="mb-3">
+                                            <b htmlFor="title">Song title</b>
+                                            <p>{order.song_name}</p>
+                                        </div>
+                                    </div>
+
+                                    <div className="mb-3">
+                                        <b htmlFor="lyric">Lyric</b>
+                                        <div className="input-group">
+                                            <textarea
+                                                id="lyric"
+                                                rows={15}
+                                                cols={200}
+                                                value={order.lyric}
+                                                onChange={(e) => handleLyricChange(e, index)}
+                                                style={{ borderRadius: '10px' }}
+                                                required
+                                            />
+                                            <div className="invalid-feedback">
+                                                Lyric is required.
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="mb-3">
+                                        <b htmlFor="artist">Artist</b>
+                                        <p>{order.artist_name}</p>
+                                    </div>
+
+                                    <div className="mb-3">
+                                        <b htmlFor="duration" className="form-label">Duration</b>
+                                        <p style={{ color: 'red' }}>{moment(order.duration).format('YYYY - MM - DD  HH:mm:ss')}</p>
+                                    </div>
+
+                                    <div className="row">
+                                        <div className="col-md-6 mb-3">
+                                            <b htmlFor="cc-genre">Genre</b>
+                                            <p>{order.genre}</p>
+                                        </div>
+                                        <div className="col-md-6 mb-3">
+                                            <b htmlFor="cc-link">Link</b>
+                                            <p>{order.link}</p>
+                                        </div>
+                                    </div>
+
+                                    <div className="mb-3 file-upload">
+                                        <label htmlFor="docxFile" className="file-upload-label">
+                                            Upload DOCX File
+                                            <div className="upload-container">
+                                                <input
+                                                    type="file"
+                                                    id="docxFile"
+                                                    accept=".docx"
+                                                    onChange={handleDocxFileChange}
+                                                />
+                                                <span className="upload-icon">&#x1F4C3;</span>
+                                                <span className="upload-text">{docxFile ? docxFileName : 'Choose a file...'}</span>
+                                            </div>
+                                        </label>
+                                    </div>
+
+                                    <div className="mb-3 file-upload">
+                                        <label htmlFor="imageFile" className="file-upload-label">
+                                            Upload Image
+                                            <div className="upload-container">
+                                                <input
+                                                    type="file"
+                                                    id="imageFile"
+                                                    accept="image/*"
+                                                    onChange={handleImageFileChange}
+                                                />
+                                                <span className="upload-icon">&#x1F4F7;</span>
+                                                <span className="upload-text">{imageFile ? imageFileName : 'Choose a file...'}</span>
+                                            </div>
+                                        </label>
+                                    </div>
+
+
+                                    {docxFile && (
+                                        <div className="mb-3 file-download">
+                                            <b>Download DOCX File:</b>
+                                            <a href={URL.createObjectURL(docxFile)} download={docxFileName}>
+                                                {docxFileName}
+                                            </a>
+                                        </div>
+                                    )}
+
+                                    {imageFile && (
+                                        <div className="mb-3 file-download">
+                                            <b>Download Image:</b>
+                                            <a href={URL.createObjectURL(imageFile)} download={imageFileName}>
+                                                {imageFileName}
+                                            </a>
+                                        </div>
+                                    )}
+
+                                    <hr className="mb-4" />
+                                    <button className="btn btn-primary btn-lg btn-block" onClick={() => navigate("/orderMusician")}>Close</button>
+                                </form>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+
+                <footer className="my-5 pt-5 text-muted text-center text-small"></footer>
             </div>
         </>
     );
